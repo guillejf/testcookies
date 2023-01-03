51581b2bc33103ed18ed63ebfe851612ece83e47
guillejf <maildeguille@gmail.com>
2023-01-03 19:21:35 -0300

    v1

--- server.js
+++ server.js
@@ -1,23 +1,64 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const app = express();
 
-app.use(cookieParser("clave secreta"));
+app.use(
+  session({
+    secret: "secreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.listen(8000, () => {
   console.log(`Example app listening on port http://localhost:8000`);
 });
 
+function auth(req, res, next) {
+  if (req.session?.user === "pepe" && req.session?.admin) {
+    return next();
+  } else {
+    return res.status(401).send("error de autorización!");
+  }
+}
+
 app.get("/", (req, res) => {
-  res
-    .cookie("name", "guille", { signed: true, httpOnly: true })
-    .send("hola mundo y te meti una cookie");
+  if (req.session.cont) {
+    req.session.cont++;
+    res.send("nos visitaste " + req.session.cont);
+  } else {
+    req.session.cont = 1;
+    res.send("nos visitaste " + 1);
+  }
 });
 
-app.get("/verlascookies", (req, res) => {
-  //   console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("abri la consola para ver si el back tiene las cookies");
-  //res.cookie("name", "guille").send("hola mundo y te meti una cookie");
+app.get("/showsession", (req, res) => {
+  res.json(req.session);
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.send("no pudo deslogear");
+    } else {
+      res.send("borramos todos quedate tranquilo que ya estas deslogeado");
+    }
+  });
+});
+
+app.get("/login", (req, res) => {
+  const { username, password } = req.query;
+  if (username !== "pepe" || password !== "pepepass") {
+    return res.send("login failed");
+  }
+  req.session.user = username;
+  req.session.admin = true;
+  res.send("login success!");
+});
+
+app.get("/informacionconfidencial", auth, (req, res) => {
+  res.send(
+    "si estas viendo esto es porque ya te logueaste, sos admin y sos pepe!"
+  );
 });
